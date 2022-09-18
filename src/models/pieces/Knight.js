50e977b47b3c5a4e266a9bbc786f665f5ff8c3e5
AlexTailor <szabbo@gmail.com>
2022-09-18 17:23:21 +0200

    Implement Knight movement

--- src/models/pieces/Knight.js
+++ src/models/pieces/Knight.js
@@ -5,4 +5,15 @@ export default class Knight extends Piece {
   constructor(player, tile) {
     super(player, icon, tile);
   }
+
+  canMove(target) {
+    const verticalDistance = this.tile.x - target.x;
+    const horizontalDistance = this.tile.y - target.y;
+
+    if (!super.canMove(target)) return false;
+    if (horizontalDistance === -2 || horizontalDistance === 2) {
+      if (verticalDistance === -1 || verticalDistance === 1) return true;
+    }
+    return false;
+  }
 }
