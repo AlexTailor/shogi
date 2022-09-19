7ea70c6e2c2bc2b18b09e2eac7b7917b1a2afbb0
AlexTailor <szabbo@gmail.com>
2022-09-19 16:13:37 +0200

    Implement gold and silver movement

--- src/models/pieces/Gold.js
+++ src/models/pieces/Gold.js
@@ -5,4 +5,22 @@ export default class Gold extends Piece {
   constructor(player, tile) {
     super(player, icon, tile);
   }
+
+  canMove(target) {
+    const backrowLeft =
+      this.player === 1
+        ? { y: this.tile.y + 1, x: this.tile.x - 1 }
+        : { y: this.tile.y - 1, x: this.tile.x - 1 };
+    const backrowRight =
+      this.player === 1
+        ? { y: this.tile.y + 1, x: this.tile.x + 1 }
+        : { y: this.tile.y - 1, x: this.tile.x + 1 };
+
+    if (!super.canMove(target)) return false;
+    if (backrowLeft.x === target.x && backrowLeft.y === target.y) return false;
+    if (backrowRight.x === target.x && backrowRight.y === target.y)
+      return false;
+    if (this.tile.isEmptyAround(target)) return true;
+    return false;
+  }
 }
--- src/models/pieces/Silver.js
+++ src/models/pieces/Silver.js
@@ -5,4 +5,15 @@ export default class Silver extends Piece {
   constructor(player, tile) {
     super(player, icon, tile);
   }
+
+  canMove(target) {
+    const leftTile = this.tile.x - 1;
+    const rightTile = this.tile.x + 1;
+    const backRow = this.player === 1 ? this.tile.y + 1 : this.tile.y - 1;
+    if (!super.canMove(target)) return false;
+    if (leftTile === target.x || rightTile === target.x) return false;
+    if (backRow === target.y && this.tile.x === target.x) return false;
+    if (this.tile.isEmptyAround(target)) return true;
+    return false;
+  }
 }
